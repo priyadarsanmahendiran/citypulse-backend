a41b5a6a242490c23a5800a49eaa26fc7f3d1305
Priyadarsan Mahendiran <m.priyan2001@gmail.com>
2025-10-26 16:52:41 +0100

    Initial setup for CityPulse

---
+++ src/main/java/com/citypulse/CityPulseApplication/CityPulseApplication.java
@@ -0,0 +1,13 @@
+package com.citypulse.CityPulseApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityPulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityPulseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/citypulse/CityPulseApplication/CityPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citypulse.CityPulseApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityPulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
